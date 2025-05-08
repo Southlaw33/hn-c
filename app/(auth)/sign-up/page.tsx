654616ade16554fa9bf113b11ac0b7900695b44d
Atchutha Rao T A <atchutha57@gmail.com>
2025-05-08 10:30:29 +0530

    feat: update import paths for NavigationBar component and add next-themes dependency

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 import Link from "next/link";
-import NavigationBar from "@/components/Navbar";
+import NavigationBar from "@/app/components/Navbar";
 const SignUpPage = () => {
   const { data } = betterAuthClient.useSession();
   const router = useRouter();
